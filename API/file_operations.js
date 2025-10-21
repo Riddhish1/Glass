841c4553d41eff2ac0814a5801991be7d58e3144
Riddhish Bonde <bonderiddhish@gmail.com>
2025-10-21 12:30:27 +0530

    file managent finish

--- API/file_operations.js
+++ API/file_operations.js
@@ -2,45 +2,74 @@ import FileOperationsManager from "../Helpers/helper.file.js";
 
 const fileManager = new FileOperationsManager();
 
-const create_file = async(req,res) =>{
+const create_file = async (req, res) => {
     //to create a new file
-    try{
+    try {
         const payload = req.body;
         const filename = payload?.filename;
         const content = payload?.content;
         const binary = payload ? payload.binary : false;
 
-        if(!filename) return res.status(500).json({error:"Please Mention File Name"});
+        if (!filename) return res.status(500).json({ error: "Please Mention File Name" });
 
-        const result = await fileManager.createFile(filename,content,binary);
+        const result = await fileManager.createFile(filename, content, binary);
         return res.json(result);
     }
-    catch(err){
-        return res.status(500).json({error:"File Creation Failed : ",err});
+    catch (err) {
+        return res.status(500).json({ error: "File Creation Failed : ", err });
     }
 }
 
 
 
 
-const modify_file = async(req,res) =>{
+const modify_file = async (req, res) => {
     //to mpdify a file
-    try{
+    try {
         const payload = req.body;
         const filename = payload?.filename;
         const content = payload?.content;
         const append = append ? payload.append : false;
 
-        if(!filename) return res.status(500).json({error:"Please Mention File Name"});
+        if (!filename) return res.status(500).json({ error: "Please Mention File Name" });
 
-        const result = await fileManager.createFile(filename,content,append);
+        const result = await fileManager.createFile(filename, content, append);
         return res.json(result);
     }
-    catch(err){
-        return res.status(500).json({error:"File modification Failed : ",err});
+    catch (err) {
+        return res.status(500).json({ error: "File modification Failed : ", err });
     }
 }
 
+const delete_file = async (req, res) => {
+    try {
+        const payload = req.body;
+        const filename = payload?.filename;
 
+        if (!filename) res.status(500).json({ error: "please enter filename" });
+
+        const result = fileManager.deleteFile(filename);
+
+        return res.json(result);
+    }
+    catch (err) {
+        return res.status(500).json({ error: "File deletion Failed : ", err });
+    }
+}
+
+const list_files = async (req, res) => {
+    try {
+        const payload = req.body;
+        const directory = payload?.directory;
+        if (!directory) res.status(500).json({ error: "please give directory" });
+        const result = fileManager.listFiles(directory);
+        return res.json(result);
+    }
+    catch (err) {
+        return res.status(500).json({ error: "Error listing files : ", err });
+    }
+}
+
+export { create_file, modify_file, delete_file, list_files };
 
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import health_check from "./API/health.js";
 import generic_command from "./API/command.js";
+import { delete_file,modify_file,create_file,list_files } from "./API/file_operations.js"; 
 
 const API_PORT = process.env.PORT || 8888;
 const API_HOST = process.env.HOST || "127.0.0.1";
@@ -26,3 +27,21 @@ app.post("/api/command",
         await generic_command(req,res);
     }
 )
+
+app.post("/api/files/create", async(req,res)=>{
+    await create_file(req,res);
+})
+
+app.post("/api/files/modify",async(req,res)=>{
+    await modify_file(req,res);
+})
+
+app.delete("/api/files/delete",
+    async(req,res)=>{
+        await delete_file(req,res);
+    }
+)
+
+app.get("/api/files/list",async(req,res)=>{
+    await list_files(req,res);
+})
